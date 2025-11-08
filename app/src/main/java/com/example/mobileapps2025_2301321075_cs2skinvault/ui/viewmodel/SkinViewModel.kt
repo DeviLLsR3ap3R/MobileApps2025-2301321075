abf68b3c21b281b9be8628e224437c3d45cdbf5c
DeviLLsR3ap3R <78639737+DeviLLsR3ap3R@users.noreply.github.com>
2025-11-08 21:25:37 +0200

    Added SkinViewModel

---
+++ app/src/main/java/com/example/mobileapps2025_2301321075_cs2skinvault/ui/viewmodel/SkinViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.mobileapps2025_2301321075_cs2skinvault.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.ViewModelProvider
+import com.example.mobileapps2025_2301321075_cs2skinvault.data.models.Skin
+import com.example.mobileapps2025_2301321075_cs2skinvault.data.repository.SkinRepository
+import kotlinx.coroutines.launch
+
+class SkinViewModel(private val repository: SkinRepository) : ViewModel() {
+    val allSkin: LiveData<List<Skin>> = repository.allSkins
+
+    fun insertSkin(skin: Skin) = viewModelScope.launch {
+        repository.insertSkin(skin)
+    }
+
+    fun updateSkin(skin: Skin) = viewModelScope.launch {
+        repository.updateSkin(skin)
+    }
+
+    fun deleteSkin(skin: Skin) = viewModelScope.launch {
+        repository.deleteSkin(skin)
+    }
+}
+
+class SkinViewModelFactory(private val repository: SkinRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SkinViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SkinViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
